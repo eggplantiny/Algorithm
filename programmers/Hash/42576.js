8fa167ec70dcbdbfce0b72e0a2ff79c866cb5b6f
eggplantiny <eggplantiny@gmail.com>
2020-10-23 21:49:28 +0900

    solving 42576

---
+++ programmers/Hash/42576.js
@@ -0,0 +1,11 @@
+function solution (participant, completion) {
+    const answer = '';
+    return answer;
+}
+
+const participant = ['mislav', 'stanko', 'mislav', 'ana']
+const completion = ['stanko', 'ana', 'mislav']
+
+const answer = solution(participant, completion)
+
+console.log(answer)
