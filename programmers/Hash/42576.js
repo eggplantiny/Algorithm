22f0e7760fc28c9435638cd01968d3bbfda401cc
eggplantiny <eggplantiny@gmail.com>
2020-10-23 22:57:58 +0900

    solved 42576

--- programmers/Hash/42576.js
+++ programmers/Hash/42576.js
@@ -1,6 +1,22 @@
 function solution (participant, completion) {
-    const answer = '';
-    return answer;
+    const dictionary = {}
+    participant.forEach(item => {
+        if (dictionary[item] === undefined) {
+            dictionary[item] = 1
+        } else {
+            dictionary[item] += 1
+        }
+    })
+
+    completion.forEach(item => {
+        dictionary[item] -= 1
+
+        if (dictionary[item].length === 0) {
+            dictionary[item] = null
+        }
+    })
+
+    return Object.keys(dictionary).filter(key => dictionary[key])[0]
 }
 
 const participant = ['mislav', 'stanko', 'mislav', 'ana']
