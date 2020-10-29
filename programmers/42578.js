0309cd8e3117bca893455b296578ccb34925dab1
eggplantiny <eggplantiny@gmail.com>
2020-10-29 21:24:17 +0900

    start solve 42578

---
+++ programmers/42578.js
@@ -0,0 +1,10 @@
+function solution (clothes) {
+    var answer = clothes.length;
+    return answer;
+}
+
+const clothes = [['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]
+
+const answer = solution(clothes)
+
+console.log(answer)
