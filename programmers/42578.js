f66b5b07c4b44b5f8e643baa442f0ee462c2160b
eggplantiny <eggplantiny@gmail.com>
2020-10-29 21:43:39 +0900

    solving 42578

--- programmers/42578.js
+++ programmers/42578.js
@@ -1,6 +1,6 @@
 function solution (clothes) {
-    var answer = clothes.length;
-    return answer;
+    const map = clothes.reduce((c, [a, b]) => (c.has(b) ? c.get(b).push(a) & null : c.set(b, [a])) || c, new Map())
+    const kinds = Array.from(map.keys())
 }
 
 const clothes = [['yellow_hat', 'headgear'], ['blue_sunglasses', 'eyewear'], ['green_turban', 'headgear']]
