55d60c5c05267690d572df69c212cad2031386ba
eggplantiny <eggplantiny@gmail.com>
2020-11-07 15:55:44 +0900

    solving 42889

--- programmers/42889.js
+++ programmers/42889.js
@@ -1,18 +1,27 @@
 function solution(N, stages) {
     const stageArray = Array(5).fill(0)
+    const successArray = Array(5).fill(0)
 
     for (let n = 0; n < stages.length; n++) {
-        const s = stages[n]
-        let c = 0
-        while (c < s) {
-            stageArray[c++] += 1
+        const stage = stages[n]
+
+        for (let c = 0; c < stage; c++) {
+            if (stageArray[c] !== undefined) {
+                stageArray[c] += 1
+            }
+
+            if (successArray[c - 1] !== undefined) {
+                successArray[c - 1] += 1
+            }
         }
-        // for (let c = 0; c < stages[n]; c++) {
-        //     stageArray[c] += 1
-        // }
     }
 
-    return stageArray;
+    const result = Array(5).fill(0)
+    for (let c = 0; c < stageArray.length; c++) {
+        result[c] = (stageArray[c] - successArray[c]) / stageArray[c]
+    }
+
+    return result.map((v, i) => { return { v, i } }).sort((a, b) => a.v - b.v < 0 ? 1 : -1).map(item => item.i + 1)
 }
 
 const stages = [2, 1, 2, 6, 2, 4, 3, 3]
